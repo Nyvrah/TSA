3126b9be58a2e8c18b640245f9fb4da386f8622f
nyvrah <harvyn.j.lim@gmail.com>
2022-12-18 21:32:45 -0500

    1

---
+++ resources/scripts/spaceshipanimation.js
@@ -0,0 +1,31 @@
+const modelViewer = document.querySelector("#spaceship");
+var x = 1;
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+window.addEventListener('scroll', function(){
+    var value = window.scrollY;
+    var yScroll = value/vh;
+    if (yScroll < 3){
+        modelViewer.style.left = "25%";
+        if (yScroll < 1){
+            modelViewer.cameraOrbit = `45deg 70deg 100%`;
+        }
+        else if (yScroll < 2){
+            modelViewer.cameraOrbit = `90deg 0deg 100%`;
+        }
+        else if (yScroll < 3){
+            modelViewer.cameraOrbit = `90deg 70deg 100%`;
+        }
+        else if (yScroll < 4){
+            modelViewer.style.left = "25%";
+            modelViewer.cameraOrbit = `0deg 70deg 100%`;
+        }
+    }
+    else {
+        modelViewer.style.left = "0%";
+        modelViewer.cameraOrbit = `0deg 60deg 80%`;
+        
+    }
+    document.getElementById('scrollCount').innerHTML= value + " " + x +" " + vh + " " + yScroll;
+    
+    x+=1;
+})
